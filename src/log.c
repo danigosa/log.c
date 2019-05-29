2fd2c245079eca58fe577bf2f36ccbd557ca2ec7
danigosa <danigosa@gmail.com>
2019-05-29 16:55:17 +0000

    format improvements

--- src/log.c
+++ src/log.c
@@ -25,6 +25,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "log.h"
 
@@ -124,14 +125,14 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
     /* Log to stderr */
     if (!L.quiet && level >= L.console_level) {
         va_list args;
-        char buf[16];
-        buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
+        char buf[32];
+        buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %Z", lt)] = '\0';
 #ifdef LOG_USE_COLOR
         fprintf(
           stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
           buf, level_colors[level], level_names[level], file, line);
 #else
-        fprintf(stderr, "%s - %-5s - %s - %d: ", buf, level_names[level], file, line);
+        fprintf(stderr, "[%s] [%d] [%s] [%s:%d] ", buf, getpid(), level_names[level], file, line);
 #endif
         va_start(args, fmt);
         vfprintf(stderr, fmt, args);
@@ -144,8 +145,8 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
     if (L.fp && level >= L.file_level) {
         va_list args;
         char buf[32];
-        buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
-        fprintf(L.fp, "%s - %-5s - %s - %d: ", buf, level_names[level], file, line);
+        buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %Z", lt)] = '\0';
+        fprintf(L.fp, "[%s] [%d] [%s] [%s:%d] ", buf, getpid(), level_names[level], file, line);
         va_start(args, fmt);
         vfprintf(L.fp, fmt, args);
         va_end(args);
